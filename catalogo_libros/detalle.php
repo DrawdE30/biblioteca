43cdefc399255c126fb5b3a6cd86fb5b2f37d825
Edward <edward91@gmail.com>
2025-04-29 20:53:06 -0500

    validaciones de login

--- catalogo_libros/detalle.php
+++ catalogo_libros/detalle.php
@@ -9,9 +9,12 @@ $libro = $resultado->fetch_assoc();
 
 <!DOCTYPE html>
 <html>
+
 <head>
+    <link rel="icon" href="../assets/img/logo.png" type="image/x-icon" />
     <title>Detalles del Libro</title>
 </head>
+
 <body>
     <h1><?php echo $libro['titulo']; ?></h1>
     <p><strong>Autor:</strong> <?php echo $libro['autor']; ?></p>
@@ -20,6 +23,7 @@ $libro = $resultado->fetch_assoc();
     <p><strong>ISBN:</strong> <?php echo $libro['isbn']; ?></p>
     <p><strong>Sinopsis:</strong> <?php echo $libro['sinopsis']; ?></p>
 
-    <a href="index.php">Volver al Catálogo</a>
+    <a href="./filtros.php">Volver al Catálogo</a>
 </body>
+
 </html>
