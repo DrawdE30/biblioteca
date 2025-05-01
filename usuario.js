85129707fbde09c92a74fdcf5a5adc30ec2b7169
Tailyslb <tglb.156@gmail.com>
2025-04-30 21:29:46 -0500

    Avanzando

--- usuario.js
+++ usuario.js
@@ -96,8 +96,8 @@ function listarUsuarios() {
                         <td>${u.usuario}</td>
                         <td>${nombresRoles}</td>
                         <td>
-                            <button class='btn btn-outline-success btn-sm' style="font-size:10px;" onclick='editar(${JSON.stringify(u)})'>✏️</button>
-                            <button class='btn btn-outline-danger btn-sm' style="font-size:10px;" onclick='eliminar(${u.idUsuario})'>❌</button>
+                            <button class='btn btn-success' style="font-size:10px;" onclick='editar(${JSON.stringify(u)})'>✏️</button>
+                            <button class='btn btn-danger' style="font-size:10px;" onclick='eliminar(${u.idUsuario})'>❌</button>
                         </td>
                     </tr>`;
             });
